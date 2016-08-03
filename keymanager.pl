f040d3bcdab21153de9d9b529a39c007e94374b2
Keedi Kim <keedi.k@gmail.com>
2016-08-03 22:32:39 +0900

    Pass url placeholder to template

--- keymanager.pl
+++ keymanager.pl
@@ -46,27 +46,37 @@ get "/key/delete" => sub {
     $c->render( template => "key-delete" );
 };
 
-=head2 /host/:name/key/delete
+=head2 /host/:host/key/delete
 
 해당 호스트의 키를 삭제하도록 서버에 요청합니다.
 
 =cut
 
-get "/host/:name/key/delete" => sub {
-    my $c = shift;
-    $c->render( template => "host-name-key-delete" );
+get "/host/:host/key/delete" => sub {
+    my $c    = shift;
+    my $host = $c->stash("host");
+
+    $c->render(
+        template => "host-name-key-delete",
+        host     => $host,
+    );
 };
 
-=head2 /user/:name/history
+=head2 /user/:user/history
 
 해당 사용자가 요청한 내역을 열람합니다.
 이 화면은 해당 사용자나 관리자만이 열람할 수 있습니다.
 
 =cut
 
-get "/user/:name/history" => sub {
-    my $c = shift;
-    $c->render( template => "user-name-history" );
+get "/user/:user/history" => sub {
+    my $c    = shift;
+    my $user = $c->stash("user");
+
+    $c->render(
+        template => "user-name-history",
+        user     => $user,
+    );
 };
 
 =head2 /history
@@ -127,12 +137,12 @@ __DATA__
 @@ host-name-key-delete.html.ep
 % layout "default";
 % title "Host Name Key Delete";
-<h1>해당 호스트 키 제거 화면</h1>
+<h1><%= $host %> 호스트 키 제거 화면</h1>
 
 @@ user-name-history.html.ep
 % layout "default";
 % title "User Name History";
-<h1>해당 사용자 히스토리</h1>
+<h1><%= $user %> 사용자 히스토리</h1>
 
 @@ history.html.ep
 % layout "default";
